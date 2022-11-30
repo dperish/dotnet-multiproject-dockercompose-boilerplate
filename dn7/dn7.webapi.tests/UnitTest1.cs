5055e223b25ae0ee7512a1801016f99f23501c5e
David Perish <davidperish@outlook.com>
2022-11-30 16:41:19 -0500

    Add tests project and update dockerfile to include in the multistage build

---
+++ dn7/dn7.webapi.tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace dn7.webapi.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
